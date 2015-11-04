235bf9233ba0274f3c7e94ab043a03f15c28fe8a
davidpodhola <david.podhola@naseukoly.cz>
2015-11-04 13:18:05 +0100

    How to download big files using the http module of FSharp.Data? 24870998

---
+++ Download Big Files 24870998/BigFilesDownloader/Program.fs
@@ -0,0 +1,17 @@
+﻿open FSharp.Data
+open System.IO
+
+module Async =
+    let AwaitTaskVoid : (System.Threading.Tasks.Task -> Async<unit>) =
+        Async.AwaitIAsyncResult >> Async.Ignore
+
+[<EntryPoint>]
+let main argv = 
+    let logoUrl = "http://infinite-download.ma.ttias.be/download.php"
+    let fileName = Path.GetTempPath() + "infinitefile.bin"
+    async {
+        let! request = Http.AsyncRequestStream(logoUrl)
+        use outputFile = new System.IO.FileStream(fileName,System.IO.FileMode.Create)
+        do! request.ResponseStream.CopyToAsync( outputFile ) |> Async.AwaitTaskVoid 
+    } |> Async.RunSynchronously
+    0
