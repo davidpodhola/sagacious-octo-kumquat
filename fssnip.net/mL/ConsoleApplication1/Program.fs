ccf5e78f06098e16d27aca977f7272ea1f8a75a9
davidpodhola <david.podhola@naseukoly.cz>
2015-11-29 18:32:42 +0100

    fixing http://www.fssnip.net/mL to the latest Suave

--- fssnip.net/mL/ConsoleApplication1/Program.fs
+++ fssnip.net/mL/ConsoleApplication1/Program.fs
@@ -1,7 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿// Run as console app or fsx
+open Suave.Http
+open Suave.Types
+open Suave.Http.Successful
+open Suave.Http.Writers
+open Suave.Http.Applicatives
+open Suave.Web
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let setCORSHeaders = 
+    setHeader  "Access-Control-Allow-Origin" "*" 
+    >>= setHeader "Access-Control-Allow-Headers" "content-type"
+
+let allow_cors : WebPart =
+    choose [
+        OPTIONS >>= 
+            fun context -> 
+                context |> (
+                    setCORSHeaders
+                    >>= OK "CORS approved" )
+    ]
+
+let webSite =
+    choose [
+        allow_cors
+        GET >>= OK "URLs are for wimps. GETting something? This is what you get."
+    ]
+
+startWebServer defaultConfig webSite
+
+(*
+=== A fiddler scratchpad to test
+
+OPTIONS http://localhost:8083/ HTTP/1.1
+User-Agent: Fiddler
+Origin: http://www.example-social-network.com
+Host: localhost:8083
+*)
+// See the 'F# Tutorial' project for more help.
