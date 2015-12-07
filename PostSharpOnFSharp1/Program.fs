f6cc18795153c2a255abb180a176291771672ea7
David Podhola <david.podhola@naseukoly.cz>
2015-12-07 22:04:41 +0100

    using postsharp with f#

---
+++ PostSharpOnFSharp1/Program.fs
@@ -0,0 +1,18 @@
+﻿open PostSharp.Patterns.Diagnostics
+open PostSharp.Extensibility
+open System
+
+module Main =
+    let log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+    [<Log>]
+    let doTheWork argv =
+        printfn "%A" argv
+        Console.ReadLine() |> ignore
+
+    [<EntryPoint>]
+    let main argv = 
+        log4net.Config.BasicConfigurator.Configure() |> ignore
+        doTheWork argv
+        0 // return an integer exit code
