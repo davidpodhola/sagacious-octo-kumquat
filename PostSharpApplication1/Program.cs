559a49636e79dabf9c458adf1594ac2c96dbca91
David Podhola <david.podhola@naseukoly.cz>
2015-12-07 12:24:18 +0100

    PostSharpApplication1 logging added

---
+++ PostSharpApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Extensibility;
+
+namespace PostSharpApplication1
+{
+    class Program
+    {
+        [Log]
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
