ed545938adb9baf3f234a8e2030a940dc2d2ce46
David Podhola <david@podhola.net>
2016-01-24 22:51:12 +0100

    testing output to console

---
+++ TestConsoleCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleCharacters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            for (char i = (char)0; i < 65536; i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            //Console.OutputEncoding = Encoding.GetEncoding(866);
+            Console.WriteLine("┌─┐");
+            Console.WriteLine("│1│");
+            Console.WriteLine("└─┘");
+            string s = "╔";
+            Console.WriteLine(s);
+        }
+    }
+}
