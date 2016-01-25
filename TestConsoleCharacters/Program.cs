7a7758fb9b0eb9edbecc41b9dce12ac8d0a85966
David Podhola <david@podhola.net>
2016-01-25 20:49:20 +0100

    showing more characters

--- TestConsoleCharacters/Program.cs
+++ TestConsoleCharacters/Program.cs
@@ -10,18 +10,21 @@ namespace TestConsoleCharacters
     {
         public static void Main(string[] args)
         {
-            /*
-            for (char i = (char)0; i < 65536; i++)
+            char c = '┐';
+            Console.WriteLine((int)c);
+            Console.WriteLine("------------------------------------");
+            for (char i = (char)9480; i < 9490; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine((int)i + ":" + i);
             }
-            */
+            Console.WriteLine("------------------------------------");
             //Console.OutputEncoding = Encoding.GetEncoding(866);
             Console.WriteLine("┌─┐");
             Console.WriteLine("│1│");
             Console.WriteLine("└─┘");
             string s = "╔";
             Console.WriteLine(s);
+            Console.ReadLine();
         }
     }
 }
