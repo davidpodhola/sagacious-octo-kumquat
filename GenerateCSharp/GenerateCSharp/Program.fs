927a14be2c825782bc52f9dac970317b4df4862d
David Podhola <david@podhola.net>
2016-03-10 12:30:05 +0100

    GenerateCSharp

---
+++ GenerateCSharp/GenerateCSharp/Program.fs
@@ -0,0 +1,10 @@
+﻿open FSharp.Data
+
+type Simple = JsonProvider<".\sample.json">
+
+let simple = Simple.Parse(".\sample.json")
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" simple.
+    0 // return an integer exit code
