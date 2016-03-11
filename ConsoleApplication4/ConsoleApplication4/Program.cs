020581455f83ef2135cf50c41a0ea8e3c288a2c3
David Podhola <david.podhola@naseukoly.cz>
2016-03-11 13:17:55 +0100

    JScript.NET

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            var sum = A.Sum();
+            for (var i = 0; i < A.Length; i++)
+            {
+                var sumLeft = A.Take(i + 1).Sum();
+                var sumRight = sum - sumLeft - A[i + 1];
+                if (sumLeft == sumRight)
+                {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { -1, 3, -4, 5, 1, -6, 2, 1 };
+            Console.WriteLine( new Solution().solution(A));
+        }
+    }
+}
---
+++ ConsoleApplication4/Library1/Library1.fs
@@ -0,0 +1,17 @@
+﻿module Library1
+
+open FsCheck
+open ConsoleApplication4
+
+[<Xunit.Fact>]
+let test =
+  let sumIs (a:int[]) =
+    let solver = Solution()
+    let index = solver.solution( a )
+    let left = a |> Array.toSeq |> Seq.take (index-1) |> Seq.toArray |> Array.sum
+    let right = a |> Array.toSeq |> Seq.skip (index + 1) |> Seq.toArray |> Array.sum
+    left = right
+  
+  Check.Quick sumIs
+  
+  
